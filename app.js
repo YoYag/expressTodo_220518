989f5810f7d703b8e8b3793532e2baf71889840d
YoYag <la2043432@gmail.com>
2022-05-18 21:59:22 +0900

    arrow function 및 express의 기본 라우팅

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// 익명함수 사용
+app.get('/a.html', function (req, res) {
+  res.send('Hi')
+})
+
+// body 안의 구문이 하나라면 중괄호{} 생략가능
+app.get('/b.html', (req, res) => res.send('Bye'))
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
